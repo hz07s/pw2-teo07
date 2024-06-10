d8baef3318133e3461bc0c3108599fdfdc52b981
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-10 13:01:04 -0500

    Enviando el objeto entero en el contexto y llamandolo como objecto dentro de test.html

--- src/listaContactos/personas/views.py
+++ src/listaContactos/personas/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Persona
 
 # Create your views here.
 def personaTextView(request):
     obj = Persona.objects.get(id = 1)
     context = {
-        'nombre' : obj.nombres,
-        'edad' : obj.edad,
+        'objeto' : obj,
     }
     return render(request, 'test.html', context)
