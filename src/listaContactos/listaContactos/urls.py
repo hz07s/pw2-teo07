bbb42b6f46605b75b04011ee0c4ef567c8a5258d
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-10 13:01:04 -0500

    Estableciendo la url para acceder a la funcion y html recientemente creado, se establecio como 'persona/'

--- src/listaContactos/listaContactos/urls.py
+++ src/listaContactos/listaContactos/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
 from inicio.views import anotherView
+from personas.views import personaTextView
 
 urlpatterns = [
     path('', myHomeView, name='Pagina de Inicio'),
     path('another/', anotherView),
     path('admin/', admin.site.urls),
+    path('persona/', personaTextView, name='otro'),
 ]
