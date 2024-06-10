5b708b3c84ece602ec99d554be3df1f9d149f9c2
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-06-10 13:00:04 -0500

    Definiendo la funcion personaTestView para mostrar mostrar nombres y apellidos de la persona con id = 2 en test.html

--- src/listaContactos/personas/views.py
+++ src/listaContactos/personas/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Persona
 
 # Create your views here.
-def myHomeView(*args, **kwargs):
-    return HttpResponse('<h1>Hola Mundo desde Django</h1>')
+def personaTextView(request):
+    obj = Persona.objects.get(id = 1)
+    context = {
+        'nombre' : obj.nomres,
+        'edad' : obj.edad,
+    }
+    return render(request, 'personas/test.html', context)
